d715bc821e8aa7bf6e9d3643614779971f6ae8fa
adelwon <kondratyuk.alinaa@gmail.com>
2022-06-09 16:19:50 +0300

    added controller with routes

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:10',
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+
+            return response()->json([
+                'error' => $errors
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($validator->passes()) {
+            $user = User::query()->create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        }
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        $user = User::query()->where('email', $request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function me(Request $request)
+    {
+        return $request->user();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('API')->group(function () {
+    Route::post('auth/register', 'AuthController@register');
+    Route::post('auth/login', 'AuthController@login');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('auth/me', 'AuthController@me');
+        Route::post('auth/logout', 'AuthController@logaut');
+    });
 });
